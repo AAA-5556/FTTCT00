de228de7b7cda631274ffe0bd65db44bd2d541ac
AAA-876876 <aolsonozeyiclang@gmail.com>
2025-12-15 23:18:50 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyblZJ3DnESNDpEKSHzqi6sEIGBgK4LdkIVOOxFmB6rHopQBKIHG-2920z1eLtkYw7r/exec";
+const API_URL = "https://corsproxy.io/?" + encodeURIComponent("https://script.google.com/macros/s/AKfycbyblZJ3DnESNDpEKSHzqi6sEIGBgK4LdkIVOOxFmB6rHopQBKIHG-2920z1eLtkYw7r/exec");
