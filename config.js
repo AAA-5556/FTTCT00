efb36a0db83790db9393579fab33e6791aca2a9b
AAA-876876 <aolsonozeyiclang@gmail.com>
2025-12-15 09:51:06 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbw64mrQrDyAJOuSIrtGhOKNLSgd-NMAXmipA3zqugI-TE_bA3qGmWjTJXe_IsUU79xgDA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyblZJ3DnESNDpEKSHzqi6sEIGBgK4LdkIVOOxFmB6rHopQBKIHG-2920z1eLtkYw7r/exec";
